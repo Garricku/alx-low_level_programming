ffde3f6d65f893df021306c60dd182acb6463170
Garrick Larson Potts <ricki.glp@gmail.com>
2023-07-13 05:12:46 -0700

    updates to the script 4-puts and exe

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
-/*
- * Main - Entry point
+/**
+ * main - Entry point
  *
- * Use the puts funtion, not the printf function
- *
- * Must return 0 always (Success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
