f3768cae7826a660d37cd54b70a029a2815655ab
Garrick Larson Potts <ricki.glp@gmail.com>
2023-07-13 05:05:21 -0700

    added c program 4-puts and a.out was created

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * Main - Entry point
+ *
+ * Use the puts funtion, not the printf function
+ *
+ * Must return 0 always (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
