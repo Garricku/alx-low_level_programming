5dfec02677976c13dfbcb0b5209b8dbe4ef4aac9
Garrick Larson Potts <ricki.glp@gmail.com>
2023-07-18 08:50:49 -0700

    added task 2 files bettification added

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the alphabet in lowercase 10 times followed by a new line
+ * print_alphabet_x10 - Prints the alphabet in lowercase ten times
  *
  * Return: Always 0 (Success)
  */
