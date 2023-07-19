74a0e37ff87fc91cbe38dd940739b81def9bcb6d
Garrick Larson Potts <ricki.glp@gmail.com>
2023-07-18 18:08:03 -0700

    corrections to task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,27 +9,30 @@
 
 void times_table(void)
 {
+	int n = 10;
 	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= n; i++)
 	{
-		_putchar('0');
+		for (j = 0; j <= n; j++)
+		{
+			int product = i * j;
 
-		for (j = 0; j <= 9; j++)
-		{
-		if ((i * j) <= 9)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(i * j + '0');
-		}
-		else
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(i * j + '0');
-		}
+			if (product < 10)
+			{
+                		_putchar(product + '0');
+			}
+			else
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+
+			if (j < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
 	 _putchar('\n');
