b33b0040ef2e8904fbc02fc48c14af3a3b7a7251
Garrick Larson Potts <ricki.glp@gmail.com>
2023-07-19 02:48:52 -0700

    task 0 debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,31 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	i = 0;
+
+	positive_or_negative(i);
+
+	if (i >= 1)
+	{
+		printf("%d is positive\n", i);
+	}
+	if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+
+	return (0);
+}
