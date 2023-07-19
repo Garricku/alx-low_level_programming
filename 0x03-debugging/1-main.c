2821e95789460edc0b458da4c869ffa3b36a21fb
Garrick Larson Potts <ricki.glp@gmail.com>
2023-07-19 03:58:46 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
         i = 0;
 
-        while (i < 10) /*this line causes the loop to be infinite*/
-        {
-                putchar(i);
-        }
+        /* while (i < 10)
+         * {
+         *       putchar(i);
+         *}*/
 
         printf("Infinite loop avoided! \\o/\n");
 
