77dea494dd10f4b6fb26a1eac1eff3dc4791ee89
Garrick Larson Potts <ricki.glp@gmail.com>
2023-07-20 05:03:06 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 - 9.
+ *
+ * @n: Number to be printed.
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	for (n = '0'; i < '10'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
