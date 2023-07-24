caf8281cd38bc7f0eef91cd5320766e04278b086
ricki.glp@gmail.com <ricki.glp@gmail.com>
2023-07-24 05:52:06 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string.
+ *
+ * @s: String to be reversed.
+ *
+ * Return: (Success)
+ */
+
+void rev_string(char *s)
+{
+	int len = 0;
+	int i;
+	char temp;
+
+	while (*s != \0)
+	{
+		len++;
+		s++;
+	}
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = *(s - i);
+		*(s - i) = *(s - len + i);
+		*(s - len + i) = temp;
+	}
+}
