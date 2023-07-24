4c1636d86ae811fa4fe11eaa2169c9d06f6d02a2
ricki.glp@gmail.com <ricki.glp@gmail.com>
2023-07-24 03:56:36 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Resets the value of integer n to 98.
+ *
+ * @n: The interger value to be reset.
+ *
+ * Return: (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	(*n = 98)
+	return (*n);
+}
