58efabf847698d998931f4c91294764fbe1c2a2c
ricki.glp@gmail.com <ricki.glp@gmail.com>
2023-07-31 10:17:18 -0700

    task 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,6 +8,15 @@
 
 void print_chessboard(char (*a)[8])
 {
-	board = a;
-	return (board);
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		
+		_putchar(a[i][j]);
+		_putchar('\n');
+		
+	}
+
 }
