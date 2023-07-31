8568ee4d32f81c3b976b8e0e62e9e46668749ff0
ricki.glp@gmail.com <ricki.glp@gmail.com>
2023-07-31 09:28:08 -0700

    task 8

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints a chessboard.
+ * @a: Array.
+ * Return: 0 (Success)
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	char lk;
+	char lp;
+	char e;
+	char up;
+	char uk;
+}
