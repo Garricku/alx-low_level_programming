361f5be75488cc77c2359818ae785de97eddddb0
ricki.glp@gmail.com <ricki.glp@gmail.com>
2023-07-31 06:14:10 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
 /**
  * _memset -  Fills memory with a constant byte.
  *
- * @*s: Pointer to memory to be used.
+ * @s: Pointer to memory to be used.
  * @b: Constant byte to be used to fill the memory.
  * @n: Number of bytes to be filled by constant byte.
  *
@@ -12,12 +12,9 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	char s[100] = {0x05};
-	b = 0x04;
-	n = 50;
 	unsigned int ch;
 
-	for (ch = 0, ch <= n, ch++);
+	for (ch = 0, ch <= n, ch++)
 	{
 		s[ch] = b;
 	}
