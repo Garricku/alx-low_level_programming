b07ec27125f7e43dde5ad6658233561ae6304629
Garrick Larson Potts <ricki.glp@gmail.com>
2023-08-02 19:40:59 +0000

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,7 +6,6 @@
  * Return: the natural square root of n, or -1 if not having a natural
  * square root.
  */
-
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
@@ -24,11 +23,8 @@ int _sqrt_recursion(int n)
  * _sqrt_helper - returns the natural square root of a number.
  * @n: the number to calculate the square root of.
  * @i: the current guess at the square root.
- *
- * Return: the natural square root of n, or -1 if n does not have a natural
- * square root.
+ * Return: the natural square root of n, or -1 if n does not have a natural.
  */
-
 int _sqrt_helper(int n, int i)
 {
 	if (i * i == n)
@@ -37,7 +33,7 @@ int _sqrt_helper(int n, int i)
 	}
 	if (i * i > n)
 	{
-        	return (-1);
+		return (-1);
 	}
 	return (_sqrt_helper(n, i + 1));
 }
