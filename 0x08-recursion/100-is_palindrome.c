8cb279022f93f72388544a1f54fa51b65a0b1a7d
ricki.glp@gmail.com <ricki.glp@gmail.com>
2023-08-02 07:21:01 -0700

    task 100

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,14 @@
+int is_palindrome(char *s)
+{
+	int start, end;
+
+	if (start >= end)
+	{
+		return 1;
+	}
+	if (s[start] != s[end])
+	{
+		return 0;
+	}
+		return (is_palindrome(s, start + 1, end - 1));
+}
