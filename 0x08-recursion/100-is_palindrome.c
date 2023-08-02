d93f9979dea33c406c38c3c708e03ad3d1067341
ricki.glp@gmail.com <ricki.glp@gmail.com>
2023-08-02 07:28:40 -0700

    task 100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,14 +1,22 @@
+#include "main.h"
+
+/**
+ * is_palindrome -  Returns 1 if a string is a palindrome and 0 if not.
+ * @s: String to be checked for palindrome.
+ * Return: (Success) 1 if palindrone else 0.
+ */
+
 int is_palindrome(char *s)
 {
 	int start, end;
 
 	if (start >= end)
 	{
-		return 1;
+		return (1);
 	}
 	if (s[start] != s[end])
 	{
-		return 0;
+		return (0);
 	}
 		return (is_palindrome(s, start + 1, end - 1));
 }
