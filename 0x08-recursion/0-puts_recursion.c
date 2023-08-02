daf6f902ecb743025cee849197dae7c81a2b9370
ricki.glp@gmail.com <ricki.glp@gmail.com>
2023-08-02 02:54:13 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string follwoed by a newline.
+ * @s: string to be printed and follwed by a newline, \n.
+ * Return: (Success)
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+	_putchar('\n');
+}
