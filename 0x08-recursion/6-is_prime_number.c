42a2342e95ff4cff8d0b696ab361308fb97e093c
ricki.glp@gmail.com <ricki.glp@gmail.com>
2023-08-02 07:02:51 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,8 +8,6 @@
 
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1)
 	{
 		return (0);
@@ -19,10 +17,10 @@ int is_prime_number(int n)
 	{
 		return (1);
 	}
-		if (n % i == 0)
-		{
-			return (0);
-		}
+	if (n % i == 0)
+	{
+		return (0);
+	}
 	}
 	return (is_prime_number(n, i + 1));
 }
