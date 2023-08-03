d1386dfd2b7934a31d1c31042e02a9440e8a6157
Garrick Larson Potts <ricki.glp@gmail.com>
2023-08-03 17:16:54 +0000

    task 100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,32 +8,29 @@
 
 int is_palindrome(char *s)
 {
-	int len = _strlen(s);
+	*ps = @s;
 
-	if (len <= 1)
+	if (s == '\0')
+	{
+		s = string;
+		break;
+	}
+	else
+	{
+		s++;
+	}
+	s--;
+	if (s < *ps)
+	{
+		s = revString;
+		break;
+	}
+	if (revString == string)
 	{
 		return (1);
 	}
-	return (pal_helper(s, len, 0));
-}
-
-/**
- * pal_helper - checks if a string is a palindrome.
- * @s: the string to check.
- * @len: the length of the string.
- * @i: the current index to check.
- * Return: 1 if s is a palindrome, 0 otherwise.
- */
-
-int pal_helper(char *s, int len, int i)
-{
-	if (i >= len / 2)
-	{
-		return (1);
-	}
-	if (s[i] != s[len - i - 1])
+	else
 	{
 		return (0);
 	}
-	return (pal_helper(s, len, i + 1));
 }
