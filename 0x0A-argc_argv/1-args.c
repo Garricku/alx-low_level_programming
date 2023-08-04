c4cf3ce4a8b3e49f8114f47a9b6b516d169866ec
ricki.glp@gmail.com <ricki.glp@gmail.com>
2023-08-04 04:47:53 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * @argc: Count arguments.
+ * @argv: Index argument.
+ * Return: (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
