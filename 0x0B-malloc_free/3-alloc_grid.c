ed9d760bdd2446ce47c97abb2312f578cc887b2c
Garrick Larson Potts <ricki.glp@gmail.com>
2023-08-07 15:30:09 +0000

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,18 +26,23 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
-	}
-	if (grid[i] == NULL)
-	{
-		free(grid[i]);
-		free(grid);
-		return (NULL);
+	
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
 	}
 	for (i = 0; i < height; i++)
 	for (j = 0; j < width; j++)
 	{
 		grid[i][j] = 0;
 	}
-	return (grid);
 	free(grid);
+	free(grid[i]);
+	return (grid);
 }
