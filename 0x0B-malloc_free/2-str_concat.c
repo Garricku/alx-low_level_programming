90b34a78b2dbf583e9edf163ea1e29caf8b67243
Garrick Larson Potts <ricki.glp@gmail.com>
2023-08-07 14:39:45 +0000

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,8 +14,6 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 	int len1;
 	int len2;
-	int i;
-	int j;
 
 	if (s1 == NULL)
 	{
@@ -25,27 +23,18 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	while (s1[len1] != '\0')
-	{
-		len1++;
-	}
-	while (s2[len2] != '\0')
-	{
-		len2++;
-	}
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
 	s = malloc(sizeof(char) * (len1 + len2 + 1));
+
 	if (s == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < len1; i++)
-	{
-		s[i] = s1[i];
-	}
-	for (j = 0; j < len2; j++)
-	{
-		s[i + j] = s2[j];
-	}
-	s[i + j] = '\0';
+
+	strcpy(s, s1);
+	strcpy(s, s2);
 	return (s);
 }
