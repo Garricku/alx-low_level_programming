9c7bb382bb0b614e011b6b77e530e29881e85bd5
Garrick Larson Potts <ricki.glp@gmail.com>
2023-08-07 16:25:52 +0000

    task 100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -38,8 +38,9 @@ char *argstostr(int ac, char **av)
 		for (j = 0; av[i][j] != '\0'; j++)
 		{
 			p[k++] = av[i][j];
-			_putchar('\n');
 		}
+		_putchar('\n');
+		
 	}
 	p[k] = '\0';
 	return (p);
