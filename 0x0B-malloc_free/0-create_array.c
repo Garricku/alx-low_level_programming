87c0703dbda588e4e67d2b96645e2f3500628001
Garrick Larson Potts <ricki.glp@gmail.com>
2023-08-07 09:42:35 +0000

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - Creates an array of chars, initializes it with a specific char.
+ * @size: Size of the array.
+ * @c: The specific chars to initialze to the array.
+ * Return: NULL if size = 0, returns a pointer to the array or NULL if it fails.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+
+	char makeArray[size] = {c};
+
+	if (size == 0)
+	{
+		return (0);
+	}
+	*p = malloc(sizeof(char) * size);
+
+	if (*p == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		return (*p);
+	}
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup -  Returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: String to duplicate.
+ * Return: NULL if str = NULL. On success, a pointer to the string.
+ */
+
+char *_strdup(char *str)
+{
