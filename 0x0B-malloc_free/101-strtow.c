2a65e31358ecf9d8fc0536f1e427177c7a51c1c4
Garrick Larson Potts <ricki.glp@gmail.com>
2023-08-08 14:09:33 +0000

    task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -5,33 +5,53 @@
 /**
  * strtow - Splits a string into words.
  * @str: String to be split into words.
- * Return: Pointer to an array of strings. 
+ * Return: Pointer to an array of strings.
  * Returns NULL if str == NULL or str == "". Fail returns NULL.
  */
 
 char **strtow(char *str)
 {
 	int i;
-	int len;
+	int count;
 	char *words;
+	char *token;
+	char *delim = "\t\n";
+	char **array;
+	int len;
 
 	if (str == NULL || str == "")
 	{
 		return (NULL);
 	}
-	for (i = 0; i != '\0'; i++)
+	words = str;
+	count = 0;
+	token = strtok(words, delim);
+	while (token != NULL)
 	{
-		if (words[i] == ' ' || i == '\t')
-		{
-			words[i] = '\n';
-		}
+		count++;
+		token = strtok(NULL, delim);
 	}
-	len = i + 1;
-	if (words == NULL)
+	array = malloc(sizeof(char *) * (count + 1));
+	if (array == NULL)
 	{
-		free(words); 
+		free(array);
 		return (NULL);
 	}
-	words = malloc(sizeof(char) * len);
-	return (words);
+	token = strtok(words, delim);
+	i = 0;
+	while (token != NULL)
+	{
+		len = strlen(token);
+		array[i] = malloc(sizeof(char) * (len + 1));
+		if (array[i] == NULL)
+		{
+			free(array[i]);
+			return (NULL);
+		}
+	strcpy(array[i], token);
+	token = strtok(NULL, delim);
+	i++;
+	}
+	array[i] = NULL;
+	return (array);
 }
