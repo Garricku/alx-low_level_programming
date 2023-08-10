f08d56a949059ee00ab9ec817f6bc2017dc78317
Garrick Larson Potts <ricki.glp@gmail.com>
2023-08-10 22:11:27 +0000

    task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -39,7 +39,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	min_size = old_size < new_size ? old_size : new_size;
-	memcopy(new_ptr, ptr, min_size);
+	memcpy(new_ptr, ptr, min_size);
 
 	free(ptr);
 
