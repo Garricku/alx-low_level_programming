59d5c17d8904d278596c7d4f5c34ce5996c4208f
Garrick Larson Potts <ricki.glp@gmail.com>
2023-08-11 12:29:15 +0000

    task 101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,39 +12,37 @@ int main(int argc, char *argv[])
 	int mul;
 	int num1;
 	int num2;
-	char *err;
-	char *str;
-	int i;
-
-	err = "Error\n";
 
 	if (argc != 3)
 	{
-		for (i = 0; err[i] != '\0'; i++)
-		{
-			_putchar(err[i]);
-		}
+		_putchar('E');
+		_putchar('r');
+		_putchar('r');
+		_putchar('o');
+		_putchar('r');
+		_putchar('\n');
+
 		exit(98);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
+
+	num1 = argv[1][0] - '0';
+	num2 = argv[2][0] - '0';
 
 	if (num1 <= 0 || num2 <= 0)
 	{
-		for(i = 0; err[i] != '\0'; i++)
-		{
-			_putchar(err[i]);
-		}
+		_putchar('E');
+		_putchar('r');
+		_putchar('r');
+		_putchar('o');
+		_putchar('r');
+		_putchar('\n');
+
 		exit(98);
 	}
-	mul = num1 * num2;
-	str = itoa(mul);
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
+	mul = num1 * num2;
+
+	printf("%d\n", mul);
 
 	return (0);
 }
