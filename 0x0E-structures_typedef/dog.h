a5eface99377d1cc6d52c4d18225210bc854a5d0
Garrick Larson Potts <ricki.glp@gmail.com>
2023-08-14 14:26:49 +0000

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * struct dog - Info on a dog.
+ * @name: Dog's name.
+ * @age: Dog's age.
+ * @owner: Dog's owner.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
