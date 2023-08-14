72369c9c9787070a1802e5814f7e80b706ff4b76
Garrick Larson Potts <ricki.glp@gmail.com>
2023-08-14 15:19:06 +0000

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ * init_dog - Intitializes a varible of type struct dog.
+ * @d: Pointer to stuct dog.
+ * @name: Name of dog.
+ * @age: Age of dog
+ * @owner: Owner of dog.
+ * Return: Void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
