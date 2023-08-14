1f8177f7ce5e7571f27644fd64f9c437113fab6c
ricki.glp@gmail.com <ricki.glp@gmail.com>
2023-08-14 09:19:23 -0700

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -19,8 +19,13 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-	return (0);
+	void **p;
+
+	p = &d;
+
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+
+	return (**p);
 }
