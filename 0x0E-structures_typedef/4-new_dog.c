c01b80c4148a1a1c6aafd293fb4e56a59c0591e3
ricki.glp@gmail.com <ricki.glp@gmail.com>
2023-08-14 11:42:50 -0700

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include "dog.h"
+
+/**
+ * new_dog - Creates a new dog.
+ * @name: Name of dog.
+ * @age: Age of dog.
+ * @owner: Owner of dog.
+ * Return: Null if fails. Or new dog.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+
+	p = malloc(sizeof(dog_t))
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+
+	return (p);
+}
