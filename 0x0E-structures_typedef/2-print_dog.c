6733b005023ae9d9162102e6f2a433afc1d819f1
Garrick Larson Potts <ricki.glp@gmail.com>
2023-08-15 11:24:42 +0000

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,11 +11,10 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
-		return (NULL);
+		return;
 	}
 
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
 	printf("Age: %.6f\n", d->age);
 	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
-	return (0);
 }
