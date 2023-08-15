03ceb280d279cc60695e74e6f657784423e574cd
Garrick Larson Potts <ricki.glp@gmail.com>
2023-08-15 15:57:49 +0000

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-
 /**
  * new_dog - Creates a new dog.
  * @name: Name of dog.
@@ -8,41 +7,32 @@
  * @owner: Owner of dog.
  * Return: Null if fails. Or new dog.
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *p;
-	int i;
-	int len1;
-	int len2;
+	int i, len1, len2;
 
 	for (i = 0; name[i] != '\0'; i++)
 	len1 = i;
-	
 	p = malloc(sizeof(dog_t));
 	if (p == NULL)
 	{
 		return (NULL);
 	}
-	
 	p->name = malloc(sizeof(char) * (len1 + 1));
-
 	if (p->name == NULL)
 	{
 		free(p->name);
 		free(p);
 		return (NULL);
 	}
-
 	for (i = 0; i < len1 && name[i] != '\0'; i++)
 	{
 		p->name[i] = name[i];
 	}
 	p->name = '\0';
-
 	for (i = 0; owner[i] != '\0'; i++)
 	len2 = i;
-
 	p->owner = malloc(sizeof(char) * (len2 + 1));
 	if (p->owner == NULL)
 	{
@@ -52,12 +42,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	for (i = 0; i < len2 && owner[i] != '\0'; i++)
-	{
 		*p->owner = owner[i];
-	}
+
 	p->owner[i] = '\0';
-
 	p->age = age;
-
 	return (p);
 }
