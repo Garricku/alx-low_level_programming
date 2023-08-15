c090c8922f2b2b0a242599a4e09b6dc9cbe7b6b1
Garrick Larson Potts <ricki.glp@gmail.com>
2023-08-15 13:34:19 +0000

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,8 +12,19 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *p;
+	int i;
 
 	p = malloc(sizeof(dog_t));
+	
+	for (i = 0; name[i] != '\0'; i++)
+	{
+		p->name = name;
+	}
+
+	for (i = 0; owner[i] != '\0'; i++)
+	{
+		p->owner = owner;
+	}
 
 	if (p == NULL)
 	{
