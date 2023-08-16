feadbf4f1f39c7cc20763cd5417ee19c06445c38
Garrick Larson Potts <ricki.glp@gmail.com>
2023-08-16 13:17:18 +0000

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name.
+ * @name: Name to be printed
+ * Return: Void.
+ */
+
+void print_name(char *name, void (*f)(char *));
+{
+	int i;
+
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	f(name);
+}
