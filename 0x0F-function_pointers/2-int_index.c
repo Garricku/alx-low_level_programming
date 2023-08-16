846bfcf321894f63110e790bcf268fb360b496e2
Garrick Larson Potts <ricki.glp@gmail.com>
2023-08-16 14:45:35 +0000

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,4 @@
-#include "function_pointer.h"
+#include "function_pointers.h"
 
 /**
  * int_index - Searches for an interger.
@@ -8,17 +8,21 @@
  * Return: Int.
  */
 
-int int_index(int *array, int size, int (*cmp)(int));
+int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
 	if ((array == NULL) || (size <= 0))
 	{
-		return (0);
+		return (-1);
 	}
 
 	for (i = 0; i < size; i++)
 	{
-		cmp(array[i]);
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
 	}
+	return (-1);
 }
