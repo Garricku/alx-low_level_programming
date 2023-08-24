cc4ae085915503b639ac38fb527ba6ad414ed200
Garrick Larson Potts <ricki.glp@gmail.com>
2023-08-24 12:57:59 +0000

    task 100

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -29,6 +29,6 @@ typedef struct list_s
 	list_t *add_node(list_t **head, const char *str);
 	list_t *add_node_end(list_t **head, const char *str);
 	void free_list(list_t *head);
-	void __atribute__((constructor)) before_main();
+	void __attribute__((constructor)) before_main();
 
 #endif
