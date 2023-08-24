311bb38f23cf1a1ed286961dc393837a2db37c2b
Garrick Larson Potts <ricki.glp@gmail.com>
2023-08-24 12:54:20 +0000

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * tort_hare - Prints the tortoise's comments before main/hare.
+ * @:
+ *
+ * Return: Void.
+ */
+
+void __atribute__((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
