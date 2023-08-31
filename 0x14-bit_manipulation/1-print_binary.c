82ec7bb8adbdb7bb9c025dc1b155ef27cb59732c
ricki.glp@gmail.com <ricki.glp@gmail.com>
2023-08-31 06:24:36 -0700

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints a number in binary.
+ * @n: The number to be printed.
+ * Return: Void.
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1;
+	int i;
+
+	for (i = 0; i < sizeof(unsigned long int) * 8; i++)
+	{
+		if ((n & (mask << (sizeof(unsigned long int) * 8 - 1 - i))) != 0)
+		{
+			break;
+		}
+	}
+
+	if (i == sizeof(unsigned long int) * 8)
+	{
+		_putchar('0');
+		return;
+	}
+
+	for (; i < sizeof(unsigned long int) * 8; i++)
+	{
+		if ((n & (mask << (sizeof(unsigned long int) * 8 - 1 - i))) != 0)
+			_putchar('1');
+		else
+			_putchar('0');
+	}
+}
