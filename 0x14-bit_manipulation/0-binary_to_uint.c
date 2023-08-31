52a90865580dffc1d28212f74a5aa0552dd8a5d5
ricki.glp@gmail.com <ricki.glp@gmail.com>
2023-08-31 06:14:21 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n = 0;
-	
+
 	if (b == NULL)
 	{
 		return (0);
@@ -17,15 +17,17 @@ unsigned int binary_to_uint(const char *b)
 
 	while (*b != '\0')
 	{
-		if (*b != '0' || *b != '1')
+		if (*b != '0' && *b != '1')
 		{
 			return (0);
 		}
 
-		n <<= 1;
+		n = n << 1;
 
 		if (*b == '1')
-		n = n | 1;
+		{
+			n = n | 1;
+		}
 
 		b++;
 	}
