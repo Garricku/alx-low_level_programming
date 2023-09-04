3dd47abaf2f22efbe6ec7e7cf012df2e150872d8
Garrick Larson Potts <ricki.glp@gmail.com>
2023-09-04 18:23:09 +0000

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,7 +27,7 @@ int main(int ac, char **av)
 	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2])
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		close(file_to);
 		exit(99);
 	}
