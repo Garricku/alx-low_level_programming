f534cd9f40190b0633fca52ac8475dcee1d3d128
ricki.glp@gmail.com <ricki.glp@gmail.com>
2023-09-29 07:38:02 -0700

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -57,6 +57,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d%s", va_arg(args, int), delim);
 		i++;
+		if (i == (n - 1))
+		{
+			printf("%d", va_arg(args, int));
+			break;
+		}
 	}
 	printf("\n");
 	va_end(args);
