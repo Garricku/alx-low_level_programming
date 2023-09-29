ed99285e117343755ac79127256dc12aef6d34eb
ricki.glp@gmail.com <ricki.glp@gmail.com>
2023-09-29 09:25:07 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,6 +3,20 @@
 #include <stdio.h>
 
 /**
+ * _strlen - Counts length of a string.
+ * @str: The string to be counted.
+ * Return: Length.
+ */
+int _strlen(const char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	return (i);
+}
+/**
  * print_all - Prints anything.
  * @*: Any charater.
  * @format: The format argument.
@@ -12,22 +26,55 @@
 
 void print_all(const char * const format, ...)
 {
-	int i;
-	char *ptr;
+	const char *ptr = format;
+	int i = 0, len = 0, flag = 0;
+	va_list args;
 
-	ptr = "*";
 	if (ptr == NULL)
-	{
 		return;
-	}
 
-	i = 0;
-	while (format[i] != format[i + 4])
+	len = _strlen(ptr);
+	va_start(args, format);
+	while (i < len)
 	{
-		while (format[i] == '%')
+		if (i > 0)
 		{
-			i++;
+			if (flag == 1)
+			{
+				printf(", ");
+				flag = 0;
+			}
 		}
+		if (ptr[i] == 'c')
+		{
+			printf("%c", va_arg(args, int));
+			flag = 1;
+			i++;
+			continue;
+		}
+		else if (ptr[i] == 'i')
+		{
+			printf("%d", va_arg(args, int));
+			flag = 1;
+			i++;
+			continue;
+		}
+		else if (ptr[i] == 's')
+		{
+			printf("%s", va_arg(args, char *));
+			flag = 1;
+			i++;
+			continue;
+		}
+		else if (ptr[i] == 'f')
+		{
+			printf("%f", va_arg(args, double));
+			flag = 1;
+			i++;
+			continue;
+		}
+		i++;
 	}
 	printf("\n");
+	va_end(args);
 }
