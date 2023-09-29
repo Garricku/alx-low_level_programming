53a5e2535cb1838c4c087ad48cb889619ee0dc92
ricki.glp@gmail.com <ricki.glp@gmail.com>
2023-09-29 05:58:15 -0700

    task 0 betty code for .h file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,12 +1,19 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+/**
+ * struct forma - A structure for the format.
+ * @c: A charater.
+ * @i: And interger.
+ * @f: A float.
+ * @s: A string.
+ */
 typedef struct forma
 {
-        char c;
-        int i;
-        float f;
-        char s;
+	char c;
+	int i;
+	float f;
+	char s;
 } format;
 
 /*FUNCTION PROTOTYPES*/
